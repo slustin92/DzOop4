d1832dc05e04d042b030884db68bc88370eb2e78
Sergey Lystin <slustin92@mail.ru>
2024-03-22 22:08:08 +0400

    .\src\

---
+++ src/main/java/ru/gb/oseminar/view/TeacherView.java
@@ -0,0 +1,18 @@
+package ru.gb.oseminar.view;
+
+
+import ru.gb.oseminar.data.Teacher;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+public class TeacherView implements UserView<Teacher>{
+    Logger logger = Logger.getLogger(TeacherView.class.getName());
+    @Override
+    public void sendOnConsole(List<Teacher> teachers) {
+        for(Teacher user: teachers){
+            logger.info(user.toString());
+        }
+
+    }
+}
